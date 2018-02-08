472cc47ca7dff6d4271fe51a5a295aed16359cbb
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-02-07 18:05:13 -0800

    MVP

--- main.js
+++ main.js
@@ -9,14 +9,22 @@ function getCardData() {
       dataType: 'json',
       beforeSend: (xhr) => {
         xhr.setRequestHeader('X-Mashape-Key', 'VwgyaXdYQPmshEYddTynCDckJIAxp1A7vHHjsn1rMIzl3e9c3g');
-        // xhr.setRequestHeader('X-Mashape-Authorization', 'mXtnPm3ltOmshc9dQJjtVdKzfnhbp14UZncjsnfzwvp6uLiMwH');
+      },
+    });
+  } else if (data) {
+    data = $.ajax({
+      url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/Neutral?health=3&collectible=1',
+      type: 'GET',
+      dataType: 'json',
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-Mashape-Key', 'VwgyaXdYQPmshEYddTynCDckJIAxp1A7vHHjsn1rMIzl3e9c3g');
       },
     });
   }
   return data;
 }
 
-function showRandomCard() {
+function showRandomCards() {
   const obj1 = cards[Math.floor(Math.random() * cards.length)];
   const obj2 = cards[Math.floor(Math.random() * cards.length)];
   const obj3 = cards[Math.floor(Math.random() * cards.length)];
@@ -25,37 +33,17 @@ function showRandomCard() {
   $('#card-image3').attr('src', obj3.img);
 }
 
-function addCard1() {
+function addCard(string) {
   if (document.getElementById('list').childElementCount < 30) {
     const image = document.createElement('img');
-    $(image).attr('src', document.getElementById('card-image1').getAttribute('src'));
+    $(image).attr('src', document.getElementById(string).getAttribute('src'));
     $(image).css('width', '20%');
     document.getElementById('list').appendChild(image);
   } else {
     alert('Warning: If you used this deck builder correctly, you\'re probably not be a good friend. Oh and your deck is done!');
   }
 }
-function addCard2() {
-  if (document.getElementById('list').childElementCount < 30) {
-    const image = document.createElement('img');
-    $(image).attr('src', document.getElementById('card-image2').getAttribute('src'));
-    $(image).css('width', '20%');
-    document.getElementById('list').appendChild(image);
-  } else {
-    alert('Warning: If you used this deck builder correctly, you\'re probably not be a good friend. Oh and your deck is done!');
-  }
-}
-function addCard3() {
-  if (document.getElementById('list').childElementCount < 30) {
-    const image = document.createElement('img');
-    $(image).attr('src', document.getElementById('card-image3').getAttribute('src'));
-    $(image).css('width', '20%');
-    document.getElementById('list').appendChild(image);
-  } else {
-    alert('Warning: If you used this deck builder correctly, you\'re probably not be a good friend. Oh and your deck is done!');
-  }
-}
-
+// Use this function if you want to pull all cards from the api, since that request is nested differently.
 // function flattenCards(data) {
 //   const result = [];
 //   for (var set in data) {
@@ -69,23 +57,17 @@ function addCard3() {
 // getCardData();
 
 $(document).ready(() => {
-  getCardData()
-    .done((data) => {
-      cards = data;
-      // cards = flattenCards(data);
-      showRandomCard();
-    });
-  // getCardDataCheater()
-  //   .done((cheaterData) => {
-  //     cards = cheaterData;
-  //     // cards = flattenCards(data);
-  //     showRandomCard();
-  //   });
-  $('#nextCard').click(showRandomCard);
-  document.getElementById('card-image1').addEventListener('click', addCard1);
-  document.getElementById('card-image1').addEventListener('click', showRandomCard);
-  document.getElementById('card-image2').addEventListener('click', addCard2);
-  document.getElementById('card-image2').addEventListener('click', showRandomCard);
-  document.getElementById('card-image3').addEventListener('click', addCard3);
-  document.getElementById('card-image3').addEventListener('click', showRandomCard);
+  getCardData().done((data) => {
+    cards = data;
+    // cards = flattenCards(data);
+    showRandomCards();
+  });
+  document.getElementById('cheater').addEventListener('click', () => { getCardData().done((data) => { cards = data; });
+  });
+  document.getElementById('card-image1').addEventListener('click', () => { addCard('card-image1'); });
+  document.getElementById('card-image1').addEventListener('click', showRandomCards);
+  document.getElementById('card-image2').addEventListener('click', () => { addCard('card-image2'); });
+  document.getElementById('card-image2').addEventListener('click', showRandomCards);
+  document.getElementById('card-image3').addEventListener('click', () => { addCard('card-image3'); });
+  document.getElementById('card-image3').addEventListener('click', showRandomCards);
 });
